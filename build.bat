151efe7fd9c27abbffebc495dddf2986875c6b13
Alistair Singh <alistair.singh7@gmail.com>
2016-05-27 23:18:06 +0200

    added more restores

--- build.bat
+++ build.bat
@@ -1,1 +1,13 @@
-csc /nologo /nostdlib /noconfig /debug:full /r:packages\System.Threading.4.0.11-beta-23516\ref\dotnet5.1\System.Threading.dll /r:packages\System.Threading.Tasks.4.0.11-beta-23516\ref\dotnet5.1\System.Threading.Tasks.dll /r:packages\System.Runtime.Extensions.4.0.11-beta-23516\ref\dotnet5.1\System.Runtime.Extensions.dll /r:packages\System.Runtime.4.0.21-beta-23516\ref\dotnet5.1\System.Runtime.dll /r:packages\System.Console.4.0.0-beta-23516\ref\dotnet5.1\System.Console.dll /out:app\helloworld.exe HelloWorld.cs  
+mkdir app
+csc ^
+  /nologo ^
+  /nostdlib ^
+  /noconfig ^
+  /debug:full ^
+  /r:packages\System.Threading.4.0.11-rc2-24027\ref\netstandard1.3\System.Threading.dll ^
+  /r:packages\System.Threading.Tasks.4.0.11-rc2-24027\ref\netstandard1.3\System.Threading.Tasks.dll ^
+  /r:packages\System.Runtime.Extensions.4.1.0-rc2-24027\ref\netstandard1.3\System.Runtime.Extensions.dll ^
+  /r:packages\System.Runtime.4.1.0-rc2-24027\ref\netstandard1.3\System.Runtime.dll ^
+  /r:packages\System.Console.4.0.0-rc2-24027\ref\netstandard1.3\System.Console.dll ^
+  /out:app\HelloWorld.exe ^
+  HelloWorld.cs  
