9763048332441927a8c117e50b096aaa42c147f7
Alistair Singh <alistair.singh7@gmail.com>
2016-05-27 23:01:18 +0200

    fixed restore packages

--- restore.bat
+++ restore.bat
@@ -5,7 +5,7 @@ set NUGET=%NUGET_DIR%\nuget.exe
 set PACKAGEDIR=%~dp0packages\
 
 mkdir %NUGET_DIR%
-if not exist %NUGET% (powershell -Command "wget http://nuget.org/nuget.exe -OutFile %NUGET%")
+if not exist %NUGET% (powershell -Command "wget http://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile %NUGET%")
 
 %NUGET% restore packages.config -Source https://api.nuget.org/v3/index.json -PackagesDirectory %PACKAGEDIR%
 
